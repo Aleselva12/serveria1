bf2c9f72a020f088857863acab7e7ce7d884a99e
Aleselva12 <aleselva97@gmail.com>
2026-09-20 01:51:59 +0200

    Remove obsolete Groq compatibility shim

--- langchain_groq.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-from langchain_ollama import ChatOllama
-
-def ChatGroq(*args, model=None, api_key=None, temperature=0, **kwargs):
-    return ChatOllama(
-        model=os.getenv("OLLAMA_MODEL", "gpt-oss:20b"),
-        base_url=os.getenv("OLLAMA_BASE_URL", "http://localhost:11435"),
-        temperature=temperature,
-    )
